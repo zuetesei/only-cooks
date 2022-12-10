be388e7cbb0a1dc1b058b0225ed1e5af1c86055a
zue <zuleikatesei@gmail.com>
2022-12-10 15:15:36 -0800

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/onlycook', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/only-cooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
